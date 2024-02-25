54f80d843fb7e28a39c2cc286dcb3badfe923690
Maksim <maksim.klimenko.mail@gmail.com>
2024-02-25 17:23:54 +0300

    Function for checking is voice channel busy for now

--- src/PlasmoVoice.ts
+++ src/PlasmoVoice.ts
@@ -11,180 +11,216 @@ import Utils from "./Utils";
 export let debug = false;
 
 export default class PlasmoVoice {
+  // System variables
+  private readonly bot: Bot;
 
-    // System variables
-    private readonly bot: Bot;
+  // Class initialization
+  constructor(bot: Bot) {
+    this.bot = bot;
 
-    // Class initialization
-    constructor(bot: Bot) {
-        this.bot = bot;
+    // Initialize packet manager
+    PacketManager.init(this.bot);
+    // Initialize voice server
+    VoiceServer.init(this.bot);
 
-        // Initialize packet manager
-        PacketManager.init(this.bot);
-        // Initialize voice server
-        VoiceServer.init(this.bot);
+    // Listen plugin channels
+    this.bot._client.on(
+      "plasmo:voice/v2",
+      async (packet: { id: string; data: any }) => {
+        Utils.debug(`[plasmo:voice/v2] Recieved ${packet.id}`);
 
-        // Listen plugin channels
-        this.bot._client.on("plasmo:voice/v2", async (packet) => {
-            
-            Utils.debug(`[plasmo:voice/v2] Recieved ${packet.id}`);
+        if (packet.id == "PlayerInfoRequestPacket") {
+          // PlayerInfoPacket
+          Utils.debug("[plasmo:voice/v2] Sending PlayerInfoPacket");
+          this.bot._client.writeChannel("plasmo:voice/v2", {
+            id: "PlayerInfoPacket",
+            data: {
+              voiceDisabled: false,
+              microphoneMuted: false,
+              minecraftVersion: bot.version,
+              version: "2.0.3",
+              publicKey: PacketManager.publicKey,
+            },
+          });
+          return;
+        } else if (packet.id == "ConnectionPacket") {
+          const data: ConnectionPacket = packet.data;
 
-            if (packet.id == 'PlayerInfoRequestPacket') {
-                // PlayerInfoPacket
-                Utils.debug("[plasmo:voice/v2] Sending PlayerInfoPacket");
-                this.bot._client.writeChannel("plasmo:voice/v2",
-                    {
-                        "id": "PlayerInfoPacket",
-                        "data": {
-                            voiceDisabled: false,
-                            microphoneMuted: false,
-                            minecraftVersion: bot.version,
-                            version: "2.0.3",
-                            publicKey: PacketManager.publicKey
-                        }
-                    }
-                );
-                return;
-            } else if (packet.id == 'ConnectionPacket') {
-                const data: ConnectionPacket = packet.data;
+          // Create voice server
+          if (data.ip == "0.0.0.0") {
+            await VoiceServer.connect(
+              Utils.getHost(this.bot),
+              data.port,
+              data.secret
+            );
+          } else {
+            await VoiceServer.connect(data.ip, data.port, data.secret);
+          }
 
-                // Create voice server
-                if (data.ip == "0.0.0.0") {
-                    await VoiceServer.connect(Utils.getHost(this.bot), data.port, data.secret);
-                } else {
-                    await VoiceServer.connect(data.ip, data.port, data.secret);
-                }
+          return;
+        } else if (packet.id == "ConfigPacket") {
+          const data: ConfigPacket = packet.data;
 
-                return;
-            } else if (packet.id == 'ConfigPacket') {
-                const data: ConfigPacket = packet.data;
+          // Save data from this packet
+          PacketManager.configPacketData = data;
+          PacketManager.aesKey = await PacketManager.getAESKey();
 
-                // Save data from this packet
-                PacketManager.configPacketData = data;
-                PacketManager.aesKey = await PacketManager.getAESKey();
+          // Check for correct encryption
+          if (data.hasEncryptionInfo == false) {
+            throw new Error(`Encryption is disabled`);
+          } else if (data.encryptionInfo.algorithm != "AES/CBC/PKCS5Padding") {
+            throw new Error(
+              `Unsupported encryption type "${data.encryptionInfo.algorithm}"`
+            );
+          }
 
-                // Check for correct encryption
-                if (data.hasEncryptionInfo == false) {
-                    throw new Error(`Encryption is disabled`);
-                } else if (data.encryptionInfo.algorithm != "AES/CBC/PKCS5Padding") {
-                    throw new Error(`Unsupported encryption type "${data.encryptionInfo.algorithm}"`);
-                }
+          this.bot.emit("voicechat_connected");
 
-                this.bot.emit("voicechat_connected");
-                
-                return;
-            } else if (packet.id == 'SourceInfoPacket') {
-                const data: SourceInfoPacket = packet.data;
+          return;
+        } else if (packet.id == "SourceInfoPacket") {
+          const data: SourceInfoPacket = packet.data;
 
-                // Don't save a player, if it's exists
-                if (PacketManager.sourceById.some(item => item.playerName == data.playerInfo.playerNick)) {
-                    return;
-                }
+          // Don't save a player, if it's exists
+          if (
+            PacketManager.sourceById.some(
+              (item) => item.playerName == data.playerInfo.playerNick
+            )
+          ) {
+            return;
+          }
 
-                PacketManager.sourceById.push({
-                    "sourceId": data.id,
-                    "playerName": data.playerInfo.playerNick
-                })
+          PacketManager.sourceById.push({
+            sourceId: data.id,
+            playerName: data.playerInfo.playerNick,
+          });
 
-                Utils.debug(PacketManager.sourceById);
+          Utils.debug(PacketManager.sourceById);
 
-                return;
-            } else if (packet.id == 'SourceAudioEndPacket') {
-                const data: SourceAudioEndPacket = packet.data;
+          return;
+        } else if (packet.id == "SourceAudioEndPacket") {
+          const data: SourceAudioEndPacket = packet.data;
 
-                const sourceData = PacketManager.sourceById.find(item => Utils.objectEquals(item.sourceId, data.sourceId));
-                if (!sourceData) {
-                    Utils.debug(`Unknown sourceId in SourceAudioEndPacket`);
-                    return;
-                }
-                
-                this.bot.emit("voicechat_voice_end", {
-                    "player": sourceData.playerName,
-                    "sequenceNumber": data.sequenceNumber
-                })
+          const sourceData = PacketManager.sourceById.find((item) =>
+            Utils.objectEquals(item.sourceId, data.sourceId)
+          );
+          if (!sourceData) {
+            Utils.debug(`Unknown sourceId in SourceAudioEndPacket`);
+            return;
+          }
 
-                return;
-            }
+          this.bot.emit("voicechat_voice_end", {
+            player: sourceData.playerName,
+            sequenceNumber: data.sequenceNumber,
+          });
 
-            Utils.debug(`[plasmo:voice/v2] Skipped ${packet.id}`);
-        })
-    }
-
-    async sendAudio(file: string, distance: number = 16, speed: number = 1.0, isStereo: boolean = false): Promise<void> {
-        if (!fs.existsSync(file)) {
-            throw new Error("File not found");
+          return;
         }
 
-        Utils.debug("[sendAudio] Converting given soundfile to PCM")
-        var pcmBuffer = await VoiceRecoder.convertToPCM(file, PacketManager.configPacketData.captureInfo.sampleRate, speed, isStereo);
-        VoiceServer.sendPCM(pcmBuffer, distance, isStereo);
+        Utils.debug(`[plasmo:voice/v2] Skipped ${packet.id}`);
+      }
+    );
+  }
+
+  async isVoiceChannelBusy() {
+    return Date.now() - VoiceServer.voiceLastTimestamp < 3 * 1.5;
+  }
+
+  async sendAudio(
+    file: string,
+    distance: number = 16,
+    speed: number = 1.0,
+    isStereo: boolean = false
+  ): Promise<void> {
+    if (!fs.existsSync(file)) {
+      throw new Error("File not found");
     }
 
-    async sendPCM(file: string, distance: number = 16, isStereo: boolean = false): Promise<void> {
-        VoiceServer.sendPCM(fs.readFileSync(file), distance, isStereo);
-    }
+    Utils.debug("[sendAudio] Converting given soundfile to PCM");
+    var pcmBuffer = await VoiceRecoder.convertToPCM(
+      file,
+      PacketManager.configPacketData.captureInfo.sampleRate,
+      speed,
+      isStereo
+    );
+    VoiceServer.sendPCM(pcmBuffer, distance, isStereo);
+  }
 
-    getSampleRate(): number {
-        return PacketManager.configPacketData.captureInfo.sampleRate;
-    }
+  async sendPCM(
+    file: string,
+    distance: number = 16,
+    isStereo: boolean = false
+  ): Promise<void> {
+    VoiceServer.sendPCM(fs.readFileSync(file), distance, isStereo);
+  }
 
-    getAllowedDistances(): number[] {
-        const proximity = PacketManager.getProximityActivation();
-        if (!proximity) { return []; }
-        return proximity?.distances;
-    }
+  getSampleRate(): number {
+    return PacketManager.configPacketData.captureInfo.sampleRate;
+  }
 
-    getDefaultDistance(): number {
-        const proximity = PacketManager.getProximityActivation();
-        if (!proximity) { return -1; }
-        return proximity?.defaultDistance;
+  getAllowedDistances(): number[] {
+    const proximity = PacketManager.getProximityActivation();
+    if (!proximity) {
+      return [];
     }
+    return proximity?.distances;
+  }
 
-    // Asked by NonemJS
-    forceConnect(): void {
-        PacketManager.registerAll();
+  getDefaultDistance(): number {
+    const proximity = PacketManager.getProximityActivation();
+    if (!proximity) {
+      return -1;
     }
+    return proximity?.defaultDistance;
+  }
 
-    /**
-     * @deprecated The method should not be used (pro-users only)
-     */
-    async _sendPacket(packetId: string, data: Object) {
-        this.bot._client.writeChannel("plasmo:voice/v2",
-            {
-                "id": packetId,
-                "data": data
-            }
-        );
-    }
+  // Asked by NonemJS
+  forceConnect(): void {
+    PacketManager.registerAll();
+  }
 
-    /**
-     * @deprecated The method should not be used (pro-users only)
-     */
-    async _sendPacketUDP(packetId: string, data: Object) {
-        const packet = await PacketManager.encodeUDP(data, packetId, VoiceServer.udpSecret);
-        VoiceServer.sendBuffer(packet);
-    }
+  /**
+   * @deprecated The method should not be used (pro-users only)
+   */
+  async _sendPacket(packetId: string, data: Object) {
+    this.bot._client.writeChannel("plasmo:voice/v2", {
+      id: packetId,
+      data: data,
+    });
+  }
 
-    /**
-     * @deprecated The method should not be used (pro-users only)
-     */
-    async _getActivationName(activationName: string) {
-        return await VoiceServer.getActivationUUID(activationName);
-    }
+  /**
+   * @deprecated The method should not be used (pro-users only)
+   */
+  async _sendPacketUDP(packetId: string, data: Object) {
+    const packet = await PacketManager.encodeUDP(
+      data,
+      packetId,
+      VoiceServer.udpSecret
+    );
+    VoiceServer.sendBuffer(packet);
+  }
 
-    async updateState(microphoneMuted: boolean = false, voiceDisabled: boolean = false) {
-        this.bot._client.writeChannel("plasmo:voice/v2",
-            {
-                "id": "PlayerStatePacket",
-                "data": {
-                    "voiceDisabled": voiceDisabled,
-                    "microphoneMuted": microphoneMuted
-                }
-            }
-        );
-    }
+  /**
+   * @deprecated The method should not be used (pro-users only)
+   */
+  async _getActivationName(activationName: string) {
+    return await VoiceServer.getActivationUUID(activationName);
+  }
 
-    enableDebug(): void {
-        debug = true;
-    }
+  async updateState(
+    microphoneMuted: boolean = false,
+    voiceDisabled: boolean = false
+  ) {
+    this.bot._client.writeChannel("plasmo:voice/v2", {
+      id: "PlayerStatePacket",
+      data: {
+        voiceDisabled: voiceDisabled,
+        microphoneMuted: microphoneMuted,
+      },
+    });
+  }
+
+  enableDebug(): void {
+    debug = true;
+  }
 }
